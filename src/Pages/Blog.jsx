a233d827d150562b2f68fdba565b314e649bb92e
Pr-ElTech <empero.lehnzy42@gmail.com>
2026-05-20 20:42:14 +0100

    created files and folder structure

---
+++ src/Pages/Blog.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <section>
+      <div>
+        <h1>Blog Page</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
