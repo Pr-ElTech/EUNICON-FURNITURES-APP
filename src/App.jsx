d91707761979cad9a3d7a0d2670cf7ad852d9769
Pr-ElTech <empero.lehnzy42@gmail.com>
2026-05-26 15:09:27 +0100

    worked on navigating between header tap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Register from "./Pages/Register";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Blog from "./Pages/Blog";
@@ -14,17 +13,24 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Authentication Routes */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<SignupPage />} />
+
+        {/* Primary Functional Content Links */}
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/product" element={<Home />} />
-        <Route path="/Checkout" element={<Checkout />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/chart" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+
+        {/* Category & Custom Services */}
+        <Route path="/category/:categoryId" element={<CategoryPage />} />
+        <Route path="/custom-furniture" element={<CustomFurniturePage />} />
+
+        {/* Utility Backstops pointing back to Home */}
+        <Route path="/cart" element={<Home />} />
         <Route path="/profile" element={<Home />} />
-        <Route path="/category/:categoryId" element={<CategoryPage />} />{" "}
-        <Route path="/CustomFurniturePage" element={<CustomFurniturePage />} />
         <Route path="/contact" element={<Home />} />
       </Routes>
     </BrowserRouter>
